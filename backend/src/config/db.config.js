c08d452143692e53c47a74d14281912b1ff2e3c6
morph1904 <morph1904@users.noreply.github.com>
2020-07-01 11:06:54 +0100

    Update backend db config and Dockerfile for Docker Deployment

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -3,15 +3,15 @@ import path from 'path';
 const config = {
         development: {
         dialect: "sqlite",
-        storage: path.join(process.cwd(), "/src", "/db", "/dev.db.sqlite")
+        storage: path.join(process.cwd(), "/src", "/dev.db.sqlite")
       },
       test: {
         dialect: "sqlite",
-        storage: path.join(process.cwd(), "/src", "/db", "/qa.db.sqlite")
+        storage: path.join(process.cwd(), "/db", "/qa.db.sqlite")
       },
       production: {
         dialect: "sqlite",
-        storage: path.join(process.cwd(), "/src", "/db", "/prod.db.sqlite")
+        storage: path.join(process.cwd(), "/db", "/prod.db.sqlite")
       }
 }
 
