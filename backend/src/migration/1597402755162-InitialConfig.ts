6eb1f8070e5ba3c5e8a93f59c7bb0814a223e2c4
morph1904 <morph1904@users.noreply.github.com>
2020-08-29 12:27:32 +0100

    move dns providers to new table, update Dockerfile

--- backend/src/migration/1597402755162-InitialConfig.ts
+++ backend/src/migration/1597402755162-InitialConfig.ts
@@ -1,5 +1,6 @@
 import {MigrationInterface, QueryRunner, getRepository} from "typeorm";
 import { Config } from "../entity/Config";
+import { DNSProvider } from "../entity/DNSProvider";
 export class InitialConfig1597402755162 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
@@ -8,10 +9,23 @@ export class InitialConfig1597402755162 implements MigrationInterface {
             config.automatic_https = true;
             config.redirect_https = true;
             config.use_dns_verification = false;
-            config.dns_provider_name = "";
+            //config.dns_provider_name = "";
             config.dns_api_token = "";
             const configRepository = getRepository(Config);
             await configRepository.save(config);
+
+            let dns = [
+                {name: "digitalocean"},
+                {name: "cloudflare"},
+                {name: "dnspod"},
+                {name: "route53"},
+                {name: "gandi"},
+            ]
+
+
+            const dnsRepository = getRepository(DNSProvider);
+            await dnsRepository.save(dns);
+            
     }
     public async down(queryRunner: QueryRunner): Promise<void> {
     }
