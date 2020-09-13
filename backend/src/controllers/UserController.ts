db7888e861fd93d2b913be67b4167d6ae65db125
Morph1904 <morph1904@gmail.com>
2020-09-13 13:36:41 +0100

    Update Type Errors

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -38,7 +38,7 @@ class UserController {
 
   static getOneById = async (req: Request, res: Response) => {
     //Get the ID from the url
-    const id: number = req.params.id;
+    const id: string = req.params.id;
 
     //Get the user from database
     const userRepository = getRepository(User);
