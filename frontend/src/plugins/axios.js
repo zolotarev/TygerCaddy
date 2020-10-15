23798a8be1d6c8bcfecff32c6ccb14331868ea3d
morph1904 <morph1904@users.noreply.github.com>
2020-10-15 10:40:42 +0100

    Update redirect on logout

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import axios from 'axios';
-import VueAxios from 'vue-axios'
-
-import store from '../store/index'
-import getEnv from '@/utils/env'
+import VueAxios from 'vue-axios';
+//import router from '../router/index';
+import store from '../store/index';
+import getEnv from '@/utils/env';
 
 
 const API_URL = getEnv('VUE_APP_API_URL')
@@ -20,6 +20,18 @@ if(API_URL){
 axios.defaults.baseURL = URL;
 axios.defaults.headers.common['Authorization'] = token;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (401 === error.response.status) {
+    store.commit('setSnack', {snack: "It looks like your token expired!", color: "error"})
+    store.dispatch('logout')
+  } else {
+      return Promise.reject(error);
+  }
+});
+
+
 const apiInstance = axios.create({
   baseURL: URL,
   headers:{
@@ -27,16 +39,7 @@ const apiInstance = axios.create({
   }
 })
 
-apiInstance.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  if (401 === error.response.status) {
-    store.commit('setSnack', {snack: "It looks like your token expired!", color: "error"})
-          window.location = '/login';
-  } else {
-      return Promise.reject(error);
-  }
-});
+
 
 Vue.prototype.$http = apiInstance;
 
