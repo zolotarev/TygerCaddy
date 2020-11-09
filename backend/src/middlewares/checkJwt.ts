8d60f572539f5a7ac08906eb378fa7617af7997e
morph1904 <morph1904@users.noreply.github.com>
2020-11-09 16:11:50 +0000

    update JWT handler

--- backend/src/middlewares/checkJwt.ts
+++ backend/src/middlewares/checkJwt.ts
@@ -31,11 +31,11 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
 
   //The token is valid for 1 hour
   //We want to send a new token on every request
-  const { userId, username } = jwtPayload;
-  const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
-    expiresIn: "1h"
-  });
-  res.setHeader("token", newToken);
+  // const { userId, username } = jwtPayload;
+  // const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
+  //   expiresIn: "1h"
+  // });
+  // res.setHeader("token", newToken);
 
   //Call the next middleware or controller
   next();
