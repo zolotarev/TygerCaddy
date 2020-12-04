17c1eaa08554cdf4ac7cf8a7f21832f8dc64764b
Lee Gregory <morph1904@gmail.com>
2020-12-04 14:58:38 +0000

    feat:(Load Balancing) Add Load balancing Panels

---
+++ frontend/src/views/LoadBalancing.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <h2 class="blue-grey--text darken-5 pa-2">All Certificates</h2>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 pa-3>
+        <load-balancing-panel></load-balancing-panel>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import LoadBalancingPanel from "../components/LoadBalancing/LoadBalancingPanel";
+export default {
+  data() {
+    return {
+      totalSteps: 100,
+    };
+  },
+  components: {
+    LoadBalancingPanel,
+  },
+  methods: {},
+};
+</script>
+<style>
+.full-height {
+  min-height: 100%;
+}
+</style>
