8fba4c66ef46bad951401c7153ad537289554b5a
morph1904 <morph1904@users.noreply.github.com>
2020-12-07 13:25:23 +0000

    feat(Versioning): Add version code to frontend and CICD

--- frontend/src/components/global/Footer.vue
+++ frontend/src/components/global/Footer.vue
@@ -1,19 +1,20 @@
 <template>
-    <v-footer class="orange" app inset>
-        <v-layout row wrap>
-            <v-flex xs8>
-                <p class="white--text text-xs-center my-3">&copy; 2019</p>
-            </v-flex>
-                        <v-flex xs4>
-                <p class="white--text text-xs-right my-3">Version:</p>
-            </v-flex>
-        </v-layout>
-      
-    </v-footer>
+  <v-footer class="orange" app inset>
+    <v-layout row wrap>
+      <v-flex xs8>
+        <p class="white--text text-xs-center my-3">&copy; 2019</p>
+      </v-flex>
+      <v-flex xs4>
+        <p class="white--text text-xs-right my-3">Version: {{ currentVersion }}</p>
+      </v-flex>
+    </v-layout>
+  </v-footer>
 </template>
-    <script>
-    export default {
-        
-    }
-    </script>
-    
+<script>
+import { version } from "../../version";
+export default {
+  data: () => ({
+    currentVersion: version,
+  }),
+};
+</script>
