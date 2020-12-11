5cc587e408bfda2a4e90787a1069db3f4f365be2
morph1904 <morph1904@users.noreply.github.com>
2020-12-11 14:09:16 +0000

    fix(Versioning): Fix display issue with version number on footer

--- frontend/src/components/global/Footer.vue
+++ frontend/src/components/global/Footer.vue
@@ -2,10 +2,19 @@
   <v-footer class="orange" app inset>
     <v-layout row wrap>
       <v-flex xs8>
-        <p class="white--text text-xs-center my-3">&copy; 2019</p>
+        <p class="white--text text-xs-center my-3">&copy; 2021</p>
       </v-flex>
       <v-flex xs4>
-        <p class="white--text text-xs-right my-3">Version: {{ currentVersion }}</p>
+        <p class="white--text text-right my-3">
+          Version:
+          <a
+            :href="
+              'https://gitlab.tygercaddy.co.uk/tygercaddy/TygerCaddy/-/releases#' +
+              currentVersion
+            "
+            >{{ currentVersion }}</a
+          >
+        </p>
       </v-flex>
     </v-layout>
   </v-footer>
